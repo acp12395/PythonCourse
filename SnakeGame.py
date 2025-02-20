c6e11da39da49d3d39c3059dc16cd7d1ec3f887d
acp12395 <acp12395@gmail.com>
2025-02-20 08:38:19 -0600

    Improved snake's movility

--- SnakeGame.py
+++ SnakeGame.py
@@ -3,7 +3,7 @@ import random
 import time
 from collections import deque
 
-class SNAKE():
+class Snake():
     _up = 2
     _down = 8
     _right = 6
@@ -11,79 +11,88 @@ class SNAKE():
     _stop = 5
 
     def __init__(self):
-        self._snake = turtle.Turtle()
-        self._snake.color("green")
-        self._snake.pensize(5)
         self._dir = self._stop
-        self._body = deque([list([0,0])])
-        self._size = 10
+        self._snake = deque([])
+        self._x = 0
+        self._y = 0
+        self._isLonger = False
+        self._time = 0.5
+        self._snakeGrows()
+        self._snakeGrows()
     def _goUp(self):
         if self._dir == self._right or self._dir == self._stop:
-            self._snake.left(90)
             self._dir = self._up
         elif self._dir == self._left:
-            self._snake.right(90)
             self._dir = self._up
     def _goDown(self):
         if self._dir == self._right or self._dir == self._stop:
-            self._snake.right(90)
             self._dir = self._down
         elif self._dir == self._left:
-            self._snake.left(90)
             self._dir = self._down
     def _goRight(self):
         if self._dir == self._up:
-            self._snake.right(90)
             self._dir = self._right
         elif self._dir == self._down:
-            self._snake.left(90)
             self._dir = self._right
         elif self._dir == self._stop:
             self._dir = self._right
     def _goLeft(self):
         if self._dir == self._up:
-            self._snake.left(90)
             self._dir = self._left
         elif self._dir == self._down:
-            self._snake.right(90)
             self._dir = self._left
         elif self._dir == self._stop:
-            self._snake.right(180)
             self._dir = self._left
     def updatePos(self):
-        if self._dir != self._stop:
-            self._snake.forward(20)
-            self._body.append(self._snake.pos())
-            self._snake.hideturtle()
-            self._snake.color("black","black")
-            if len(self._body) > self._size:
-                self._snake.penup()
-                self._snake.setx(self._body[0][0])
-                self._snake.sety(self._body[0][1])
-                self._snake.pendown()
-                self._snake.setx(self._body[1][0])
-                self._snake.sety(self._body[1][1])
-                self._snake.penup()
-                self._snake.setx(self._body[len(self._body) - 1][0])
-                self._snake.sety(self._body[len(self._body) - 1][1])
-                self._body.popleft()
-                self._snake.pendown()
-            self._snake.stamp()
-            self._snake.color("green","green")
-            self._snake.showturtle()
+        if self._dir == self._up:
+            self._y = self._y + 10
+        elif self._dir == self._down:
+            self._y = self._y - 10
+        elif self._dir == self._right:
+            self._x = self._x + 10
+        elif self._dir == self._left:
+            self._x = self._x - 10
+        self._move()
+        time.sleep(self._time)
+    def _snakeGrows(self):
+        snake = turtle.Turtle()
+        snake.hideturtle()
+        snake.color("green")
+        snake.penup()
+        snake.shape("square")
+        snake.speed(0)
+        snake.goto(self._x,self._y)
+        snake.showturtle()
+        self._snake.appendleft(snake)
+    def _move(self):
+        if self._isLonger == True:
+            self._snakeGrows()
+            self._isLonger = False
+        else:
+            self._snake.rotate(1)
+            self._snake[0].hideturtle()
+            self._snake[0].setx(self._x)
+            self._snake[0].sety(self._y)
+            self._snake[0].showturtle()
 
 
 
-class MOUSE():
+
+
+class Mouse():
     def __init__(self):
-        self._x = (int(random.random()*24) - 12)*20
-        self._y = (int(random.random()*24) - 12)*20
-        
         self._mouse = turtle.Turtle()
         self._mouse.hideturtle()
         self._mouse.penup()
         self._mouse.color("white")
         self._mouse.left(90)
+        self._mouse.speed(0)
+        self._changePosition()
+
+    def _changePosition(self):
+        self._x = (int(random.random()*48) - 24)*10
+        self._y = (int(random.random()*48) - 24)*10
+        self._mouse.hideturtle()
         self._mouse.setx(self._x)
         self._mouse.sety(self._y)
         self._mouse.showturtle()
@@ -91,8 +100,8 @@ class MOUSE():
 
 s = turtle.Screen()
 s.screensize(500,500,"black")
-mouse = MOUSE()
-snake = SNAKE()
+mouse = Mouse()
+snake = Snake()
 
 s.listen()
 s.onkeypress(snake._goUp, "Up")
@@ -103,7 +112,5 @@ s.onkeypress(snake._goLeft, "Left")
 while 1:
     s.update()
     snake.updatePos()
-    time.sleep(.10)
-
 
 turtle.done()
